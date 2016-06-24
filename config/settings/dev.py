03a699b50eb23ed6e2a146a2da514a7c43bd0e3a
Dean Mercado <dean@yeti.co>
2016-06-24 16:06:33 -0700

    Add all the files

---
+++ config/settings/dev.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "{{ lookup('vault', 'secret/yurttest_development', 'secret_key', 'vault_one_true_vault.json') }}"
+
+ALLOWED_HOSTS = ["162.243.112.16", ]
---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "C;Vy*Gt1S3dgI;Yl-Z46><cF/A]Ed5D#Cs,M2aOt"
+
+ALLOWED_HOSTS = ['*']
